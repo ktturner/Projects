d109fdffee358e710bd412f32e6e325c971a2442
Parker A Tew <ptew@mit.edu>
2016-04-12 22:17:14 -0400

    different version of Temperature.js

--- Temperature.js
+++ Temperature.js
@@ -1,18 +1,19 @@
 
-
 function coat(temperature, rain){
+	if 
 	if (temperature < 0 || rain == 'yes') {
 		document.write('Stay inside!')
-}
-else if (temperature < 30 || rain == 'no') {
-		document.write('Please wear a coat and a hat. You need to stay warm!');
-}
-else if (temperature < 50 || rain == 'no'){
-	document.write('Please put on a coat. It is cold outside!');
-}
-else {
-	document.write('It\'s nice outside, wear what you want.')
-}
+	} else {
+		if (temperature < 30 ) {
+			document.write('Please wear a coat and a hat. You need to stay warm!');
+		}
+		else if (temperature < 50){
+			document.write('Please put on a coat. It is cold outside!');
+		}
+		else {
+			document.write('It\'s nice outside, wear what you want.')
+		}
+	}
 }
 
 coat(50,'yes');
