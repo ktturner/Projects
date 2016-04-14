8fd1b22ab701f14c6ce9e859247323cfc21d3404
ktturner <kturner02@gmail.com>
2016-04-13 22:16:10 -0400

    Update Temperature.js

--- Temperature.js
+++ Temperature.js
@@ -3,6 +3,9 @@ function coat(temperature, rain){
 	if 
 	if (temperature < 0 || rain == 'yes') {
 		document.write('Stay inside!')
+		/* I see what you did here. You took the || rain == 'no' out by making a larger if else statement. I don't know 
+		all of the technical jargon to explain what is happening, but I do get it. Thanks for showing me a different
+		way to think about this problem!*/
 	} else {
 		if (temperature < 30 ) {
 			document.write('Please wear a coat and a hat. You need to stay warm!');
